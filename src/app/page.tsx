928de2684be2e3f2332a1416f30a1acf2070da9b
Dan <dqdo873@gmail.com>
2024-11-24 18:19:56 -0500

    Elowyn's intregration for create restaurant and list restaurant in GUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,16 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import { useRouter } from 'next/navigation';
+import { Restaurant } from '../model'
+
+import axios from "axios";
+
+
+// all WEB traffic using this API instance. You should replace this endpoint with whatever
+// you developed for the tutorial and adjust resources as necessary.
+const instance = axios.create({
+  baseURL: 'https://xx0uqht4q7.execute-api.us-east-2.amazonaws.com/12'
+});
 
 function Login({ role, closeLogin }) {
 
@@ -20,12 +30,12 @@ function Login({ role, closeLogin }) {
     <div className="enterCredentials">
       <label>{role} Login</label>
       <div className="emailAddressContainer">
-        <label htmlFor={`${role}EmailAddress`}>E-mail Address: </label>
+        <label htmlFor={`${role}EmailAddress`}>ID: </label>
         <input id={`${role}EmailAddress`} type="email" style={{ borderColor: 'black', borderWidth: '2px', borderStyle: 'solid' }} placeholder="email@address.com" />
       </div>
       <div className="passwordContainer">
         <label htmlFor={`${role}Password`}>Password: </label>
-        <input id={`${role}Password`} type="password" style={{ borderColor: 'black', borderWidth: '2px', borderStyle: 'solid' }} />
+        <input id={`${role}passsword`} type="password" style={{ borderColor: 'black', borderWidth: '2px', borderStyle: 'solid' }} placeholder="password" />
       </div>
       <button className="button closeLogin" onClick={closeLogin} style={{ marginRight: '50px' }}>
         Close
@@ -38,13 +48,123 @@ function Login({ role, closeLogin }) {
   );
 }
 
+
+
+
+
+
+
+
 export default function Home() {
   const [loginRole, setLoginRole] = useState<string | null>(null);;
   const [redraw, forceRedraw] = React.useState(0)
+  const [constants, setConstants] = React.useState('')   // state from the AWS API
+  const [restaurantName, setRestaurantName] = useState('');
+  const [restaurantLocation, setRestaurantLocation] = useState('');
+  const [RetrieveRestaurant, setRetrieveRestaurant] = useState('');
+  const [restaurantNameList, setRestaurantNameList] = useState<string[]>([]);
+  // Whenever 'redraw' changes (and there are no loaded constants) this fetches from API
+  React.useEffect( () => {
+    if (!constants) {
+      retrieveConstants(setConstants)
+      console.log("constants:", constants)
+    }
+  }, [redraw])
 
   function andRefreshDisplay() {
     forceRedraw(redraw + 1)
   }
+
+
+
+
+  function retrieveConstants(): string {
+    let constantsString = '';
+    instance.get('/restaurants')
+      .then(function (response) {
+        let status = response.data.statusCode;
+  
+        if (status == 200) {
+          // Concatenate the constant names and values into a string
+          for (let con of response.data.constants) {
+
+            if(restaurantName==con.restaurant_name){
+            constantsString = `password: ${con.password}    ID: ${con.restaurant_id}`; // Adjust format as needed
+            }
+          }
+        }
+       
+ 
+      setRetrieveRestaurant(constantsString)
+       return constantsString
+  
+      })
+      .catch(function (error) {
+        console.log(error);
+        return ""
+      });
+  
+  
+  
+    // Return the constants string
+  }
+
+
+  function retrieveRestaurants(): string {
+    let constantsString = '';
+    instance.get('/restaurants')
+      .then(function (response) {
+        let status = response.data.statusCode;
+  
+        if (status == 200) {
+          // Concatenate the constant names and values into a string
+          for (let con of response.data.constants) {
+       
+            const restaurantList = response.data.constants.filter(con => "active"==con.status).map(con => con.restaurant_name);
+            
+            setRestaurantNameList(restaurantList);
+          }
+        }
+       
+ 
+      setRetrieveRestaurant(constantsString)
+       return constantsString
+  
+      })
+      .catch(function (error) {
+        console.log(error);
+        return ""
+      });
+  
+  
+  
+    // Return the constants string
+  }
+
+
+
+
+
+
+
+
+  function createConstant() {
+   
+    // potentially modify the model
+  
+    if (restaurantName && restaurantLocation) {
+    
+      instance.post('/restaurants', { name: restaurantName, location: restaurantLocation })
+      .then(function (response) {
+
+        andRefreshDisplay()
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    }
+  }
+  
 
   const showLogin = (role: string) => {
     setLoginRole(role);
@@ -54,8 +174,11 @@ export default function Home() {
     setLoginRole(null);
   };
 
+  
+
   return (
     <div>
+  
       <div className="loginButtonContainer">
         <button data-testid="loginAdministrator" className="button loginAdministrator" onClick={() => showLogin('Administrator')}>
           Login Administrator
@@ -63,12 +186,26 @@ export default function Home() {
         <button data-testid="loginManager" className="button loginManager" onClick={() => showLogin('Manager')}>
           Login Manager
         </button>
-
         {loginRole && <Login role={loginRole} closeLogin={closeLogin} />}
+      </div>
+  
+      <button className="button" onClick={() => retrieveRestaurants()}>List Restaurants</button>
+
+      <h1>Create Restaurant</h1>
+      name: <input className="text" value={restaurantName} onChange={(e) => setRestaurantName(e.target.value)} />&nbsp;
+      location: <input className="text" value={restaurantLocation} onChange={(e) => setRestaurantLocation(e.target.value)} />&nbsp;
+      
+      <button className="button" onClick={createConstant}>Create</button><p></p>    
+      <label className="score">{ RetrieveRestaurant}</label>
+
+      <label className="Title">{"restaurant List"}</label>
+      <label className="Restaurants">
+
+  {restaurantNameList.map((name, index) => (
+    <div key={index}>{name}</div>
+  ))}
+</label>
 
       </div>
-
-      <button className="button">List Restaurants</button>
-    </div>
   );
 }
