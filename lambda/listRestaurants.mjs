1bb0d79e6c88e0be0a525f8c18d988556cb927fb
Dan <dqdo873@gmail.com>
2024-11-24 18:26:31 -0500

    Elowyn's intregration for list restaurant

---
+++ lambda/listRestaurants.mjs
@@ -0,0 +1,24 @@
+import mysql from 'mysql'
+
+export const handler = async (event) => {
+  
+  let ListRestaurants = () => {
+      return new Promise((resolve, reject) => {
+          pool.query("SELECT * FROM restaurants", [], (error, rows) => {
+              if (error) { return reject(error); }
+              return resolve(rows);
+          })
+      })
+  }
+
+  const all_constants = await ListRestaurants()
+  
+  const response = {
+    statusCode: 200,
+    constants: all_constants
+  }
+  
+  pool.end()     // close DB connections
+
+  return response;
+}
